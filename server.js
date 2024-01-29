39f9edecaa9a847bde4046633b18f85b81f73fa1
oscar <oscarmuga2008@gmail.com>
2024-01-29 08:20:36 +0100

    commit primer dia de proyecto

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const app = express();
 const port = 3745;
+const CryptoJS = require("crypto-js");
+
 
 const db = mysql.createPool({
     host: 'dam.inspedralbes.cat',
@@ -33,11 +35,15 @@ app.get('/users/nearby', (req, res) => {
     const currentLongitude = parseFloat(req.query.longitude);
     const radiusInKm = parseFloat(req.query.radius) * 10 || 10.0;
 
+    console.log('currentLatitude:', currentLatitude);
+    console.log('currentLongitude:', currentLongitude);
+    console.log('radiusInKm:', radiusInKm);
+
     const query = `
         SELECT idUsu, nombreUsu, 
             ST_X(ubiUsu) AS latitude, 
             ST_Y(ubiUsu) AS longitude,
-            ST_DISTANCE_SPHERE(ubiUsu, ST_GeomFromText('POINT(${currentLongitude} ${currentLatitude})', 4326)) AS distance,
+            ST_DISTANCE_SPHERE(ubiUsu, ST_GeomFromText('POINT(${currentLongitude} ${currentLatitude})', 4326))/1000 AS distance,
             imageUrl
         FROM USUARIO
         HAVING distance <= ${radiusInKm}
@@ -50,6 +56,7 @@ app.get('/users/nearby', (req, res) => {
             console.error(err);
             res.status(500).json({ error: 'Error en la consulta de usuarios cercanos' });
         } else {
+            console.log(results);
             res.json(results);
         }
     });
@@ -61,15 +68,16 @@ app.post('/users', upload.single('imagenFile'), (req, res) => {
 
     // Imprimir el cuerpo de la solicitud
     console.log('Cuerpo de la solicitud:', req.body);
-    const { name, latitude, longitude, surname, mailUsu } = req.body;
+    const { name, latitude, longitude, surname, mailUsu , pass} = req.body;
 
     console.log("name: ", name);
     console.log("latitude: ", latitude);
     console.log("longitude: ", longitude);
     console.log("surname: ", surname);
     console.log("mailUsu: ", mailUsu);
+    console.log("pass: ", pass);
 
-    if (!name || !latitude || !longitude || !surname || !mailUsu) {
+    if (!name || !latitude || !longitude || !surname || !mailUsu || !pass) {
         console.log('Faltan datos obligatorios');
         return res.status(400).json({ error: 'Faltan datos obligatorios' });
     }
@@ -81,9 +89,9 @@ app.post('/users', upload.single('imagenFile'), (req, res) => {
     console.log('Imagen:', imageUrl);
 
     const query = `
-    INSERT INTO USUARIO (nombreUsu, ubiUsu, apellidosUsu, mailUsu, imageUrl) VALUES (?, ST_GeomFromText(?), (?), (?), ?)
+    INSERT INTO USUARIO (nombreUsu, ubiUsu, apellidosUsu, mailUsu, imageUrl, pass) VALUES (?, ST_GeomFromText(?), (?), (?), ?, ?)
 `;
-    const values = [name, location, surname, mailUsu, imageUrl];
+    const values = [name, location, surname, mailUsu, imageUrl, pass];
 
     db.query(query, values, (err, result) => {
         if (err) {
@@ -94,6 +102,108 @@ app.post('/users', upload.single('imagenFile'), (req, res) => {
         }
     });
 });
+
+app.get('/pass', (req, res) =>{
+    const pass = req.body.pass;
+
+    res.send(CryptoJS.MD5(pass).toString());
+})
+
+app.post('/login', (req, res) =>{
+    const { mailUsu, passUsu } = req.body;
+    console.log("mailUsu: ", mailUsu);
+    console.log("passUsu: ", passUsu);
+
+    if(!mailUsu || !passUsu){
+        console.log('Faltan datos obligatorios');
+        return res.status(400).json({ error: 'Faltan datos obligatorios' });
+    }else{
+        const sql = `SELECT * FROM USUARIO WHERE mailUsu = ?`;
+
+        db.query(sql, mailUsu, (err, result) => {
+            if (err) {
+                console.error('Error en la consulta de inserción:', err);
+                res.status(500).json({ error: 'Error al agregar el usuario' });
+            } else {
+                console.log(result);
+                let pass = CryptoJS.MD5(passUsu).toString();
+                console.log("Introducida: ",pass);
+                console.log("Correcta: ",result[0].pass);
+                if(res == 0 ||result[0].pass != pass){
+                    console.log('Usuario o contraseña incorrectos');
+                    res.status(400).json({ error: 'Usuario o contraseña incorrectos' });
+                }else{
+                    res.json({ result });
+                }
+            }
+        });
+    }
+})
+
+//ruta para validar si un correo ya está registrado
+app.get('/users/validateMail', (req, res) => {
+    const mailUsu = req.query.mailUsu;
+    console.log("mailUsu: ", mailUsu);
+
+    if (!mailUsu) {
+        console.log('Faltan datos obligatorios');
+        return res.status(400).json({ error: 'Faltan datos obligatorios' });
+    } else {
+        const sql = `SELECT * FROM USUARIO WHERE mailUsu = ?`;
+
+        db.query(sql, mailUsu, (err, result) => {
+            if (err) {
+                console.error('Error en la consulta de inserción:', err);
+                return res.status(500).json({ error: 'Error al agregar el usuario' });
+            } else {
+                console.log(result);
+                if (result.length === 0) {
+                    console.log('Correo no registrado');
+                    return res.status(404).json({ error: 'Correo no registrado' });
+                } else {
+                    return res.json({ result });
+                }
+            }
+        });
+    }
+});
+
+app.post("/setPass", (req,res)=>{
+    const newPass = req.body.newPass;
+    const usu = req.body.usu;
+
+    let pass = CryptoJS.MD5(newPass).toString();
+
+    const query = `UPDATE USUARIO SET pass = ? WHERE mailUsu = ?`;
+    const VALUES = [pass, usu];
+
+    db.query(query, VALUES, (err, result)=>{
+        if(err){
+            console.error('Error en la consulta de inserción:', err);
+            return res.status(500).json({ error: 'Error al agregar el usuario' });
+        }else{
+            console.log(result);
+            return res.json({ result });
+        }
+    })
+})
+
+app.get("/getPass", (req,res)=>{
+    const usu = req.query.usu;
+
+    const query = `SELECT pass FROM USUARIO WHERE mailUsu = ?`;
+    const VALUES = [usu];
+
+    db.query(query, VALUES, (err, result)=>{
+        if(err){
+            console.error('Error en la consulta de inserción:', err);
+            return res.status(500).json({ error: 'Error al agregar el usuario' });
+        }else{
+            console.log(result);
+            return res.json({ result });
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`Servidor en ejecución en http://localhost:${port}`);
